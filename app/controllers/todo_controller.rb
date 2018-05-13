71a3ab1be02e68647afea37e63c857f60b059b89
Maria Paz Perez <mzereepp@gmail.com>
2018-05-13 14:49:11 +0000

    adding new todos

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,34 +1,44 @@
 class TodoController < ApplicationController
- def index
-  @Tile="Workshop Todo List"
- end
- def show 
-  @Tile="Workshop Todo List "
-  todo_id=params[:id]
-  if todo_id == "1"
-   @todo_description='Make the curriculum'
-   @pomodoro_estimate='3'
-  elsif todo_id == '2'
-   @todo_description='buy supplies'
-   @pomodoro_estimate= '2'
-  elsif todo_id =="3"
-   @todo_description = 'Meet with the volunteer trainer'
-   @pomodoro_estimate = '1'
-  elsif  todo_id == '4'
-    @todo_description= 'Order food for Saturday and Sunday'
-    @pomodoro_estimate = '2'
-  elsif todo_id == '5'
-    @todo_description= 'Check pre-work assignments'
-    @pomodoro_estimate= '5'
-  elsif todo_id == '6'
-    @todo_description= 'send workshop location to all the students'
-    @pomodoro_estimate= '5'
-  elsif todo_id == '7'
-    @todo_description= 'have a great workshop'
-    @pomodoro_estimate = '4'
-  else
-   @todo_description='code'
  
+  def index
+   @Tile="Workshop Todo List"
   end
- end
- end 
+  
+  def show 
+   @Tile="Workshop Todo List "
+   todo_id=params[:id]
+   if todo_id == "1"
+    @todo_description='Make the curriculum'
+    @pomodoro_estimate='3'
+   elsif todo_id == '2'
+    @todo_description='buy supplies'
+    @pomodoro_estimate= '2'
+   elsif todo_id =="3"
+    @todo_description = 'Meet with the volunteer trainer'
+    @pomodoro_estimate = '1'
+   elsif  todo_id == '4'
+     @todo_description= 'Order food for Saturday and Sunday'
+     @pomodoro_estimate = '2'
+   elsif todo_id == '5'
+     @todo_description= 'Check pre-work assignments'
+     @pomodoro_estimate= '5'
+   elsif todo_id == '6'
+     @todo_description= 'send workshop location to all the students'
+     @pomodoro_estimate= '5'
+   elsif todo_id == '7'
+     @todo_description= 'have a great workshop'
+     @pomodoro_estimate = '4'
+   else
+    @todo_description='code'
+   end
+  end
+  def new 
+  end 
+  def create
+  t = Todo.new
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro_estimate']
+  t.save
+  redirect_to "/todo/show/#{ t.id }"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,8 @@ Rails.application.routes.draw do
   #     resources :products
   get 'todo/index', to:'todo#index'
   get 'todo/show/:id', to:'todo#show'
+  get 'todo/new', to: 'todo#new'
+  get 'todo:create', to: 'todo#create'
   #   end
+  
 end
