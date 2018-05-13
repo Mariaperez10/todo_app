974dbde3950c2c2f7122baa38aaaef5d8c43001d
Maria Paz Perez <mzereepp@gmail.com>
2018-05-13 18:45:08 +0000

    finished

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,13 +2,14 @@ class TodoController < ApplicationController
  
   def index
    @Tile="Workshop Todo List"
+   @todos= Todo.all
   end
   
   def show 
    @Tile="Workshop Todo List "
    @todo= Todo.find_by_id(params[:id])
-    @todo_description= @todo.description
-    @todo_pomodoro_estimate= @todo.pomodoro_estimate
+   @todo_description= @todo.description
+   @todo_pomodoro_estimate= @todo.pomodoro_estimate
   end
   def new 
   end 
@@ -22,9 +23,20 @@ class TodoController < ApplicationController
   
   def edit
    @todo = Todo.find_by_id(params[:id])
+  end
+  
+  def update
+   t = Todo.find_by_id(params[:id])
    t.description = params['description']
    t.pomodoro_estimate = params['pomodoro_estimate']
    t.save
    redirect_to "/todo/show/#{t.id}"
   end
+  
+  def destroy 
+   t= Todo.find_by_id(params[:id])
+   t.destroy
+   redirect_to "/todo/index"
+  end
+   
 end
